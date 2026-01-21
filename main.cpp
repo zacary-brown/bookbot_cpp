2f4db9fe87c74631df1ad50f9ab17edfc543462a
Zacary Brown <zacarybrown360@gmail.com>
2026-01-20 16:06:18 -0800

    Separated funcitonality out of main. Now uses a function that returns the book as a string for main to print

--- main.cpp
+++ main.cpp
@@ -1,21 +1,28 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
-int main()
+std::string get_book_text(std::string book)
 {
     std::ifstream MyFile("books/frankenstein.txt");
+    std::ostringstream text;
 
     if (!MyFile.is_open())
     {
         std::cout << "Could not open file to read" << std::endl;
-        return -1;
+        return "";
     }
 
-    std::string line;
-    while (std::getline(MyFile, line))
-    {
-        std::cout << line << std::endl;
-    }
+    text << MyFile.rdbuf();
+
+    return text.str();
+}
+
+int main()
+{
+    std::string my_text = get_book_text("books/frankenstein.txt");
+
+    std::cout << my_text << std::endl;
 
     return 0;
 }
